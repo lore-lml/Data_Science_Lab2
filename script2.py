5f5a5fa3a1c7fc6da2dcef27c20354ac15331565
Lorenzo Limoli <lorenzo.limoli@outlook.it>
2019-11-10 13:20:58 +0100

    5.

--- script2.py
+++ script2.py
@@ -32,7 +32,7 @@ def csv2list(path):
 
         for row in reader:
             dataset[COMMENT_I].append(row[0])
-            dataset[LABELS_I].append(row[1])
+            dataset[LABELS_I].append(int(row[1]))
 
         return dataset
 
@@ -42,9 +42,9 @@ def tf(tokens):
 
     for word in tokens:
         try:
-            tf[word] += 1
+            tf[word.strip()] += 1
         except KeyError:
-            tf[word] = 1
+            tf[word.strip()] = 1
 
     return tf
 
@@ -68,6 +68,30 @@ def idf(documents_freq):
     return {w: log(N/t) for w, t in dft.items()}
 
 
+def tf_idf(tf_list, idf):
+    tfidf = []
+
+    for d in tf_list:
+        tfidf_dict = {}
+        for word, freq in d.items():
+            tfidf_dict[word] = freq * idf[word]
+        tfidf.append(tfidf_dict)
+    return tfidf
+
+
+def norm(d):
+    return sum([tf_idf**2 for t, tf_idf in d.items()])**.5
+
+
+def dot_product(d1, d2):
+    word_set = set(list(d1.keys()) + list(d2.keys()))
+    return sum([(d1.get(d, 0.0) * d2.get(d, 0.0)) for d in word_set])
+
+
+def cosine_similarity(d1, d2):
+    return dot_product(d1, d2) / (norm(d1) * norm(d2))
+
+
 if __name__ == '__main__':
     # 1.
     dataset = csv2list("data_sets/imdb.csv")
@@ -80,8 +104,19 @@ if __name__ == '__main__':
 
     # 3.
     documents_freq = tf_list(tokens)
-    # print(term_freq[:5])
+    # print(documents_freq[:1])
 
     # 4.
     inverse_document_freq = idf(documents_freq)
-    print(inverse_document_freq)
+    """for k, v in inverse_document_freq.items():
+        print(f"{k}: {v}")"""
+
+    # 5.
+    tf_idf = tf_idf(documents_freq, inverse_document_freq)
+    # print(sorted(tf_idf[0].items(), key=lambda item: item[1], reverse=True)[:10])
+
+    # 6.
+    test_document = tf_idf[0]
+    positive_d = [i for i, d in enumerate(dataset[LABELS_I]) if d == 1]
+    negative_d = [i for i, d in enumerate(dataset[LABELS_I]) if d == 0]
+    # print(f"{len(positive_d)} {len(negative_d)}")
